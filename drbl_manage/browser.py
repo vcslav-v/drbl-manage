a77c343b494fb193c2f7803eac7823c2ad9cb86e
Vcslav <vcslav.v@gmail.com>
2022-04-25 09:39:59 +0300

    fix

--- drbl_manage/browser.py
+++ drbl_manage/browser.py
@@ -12,7 +12,6 @@ from drbl_manage import db_tools
 
 
 class Browser:
-    """Browser"""
     def __init__(self, selenium_ip, antcpt=True, proxy=True) -> None:
         logger.debug(selenium_ip)
         browser_options = webdriver.chrome.options.Options()
